3c8e8a7cf15b41c38fd003d449fe2efafe913016
arielvillafuerte <arielcesarantoniovillafuerte@gmail.com>
2022-12-09 12:25:14 -0300

    routes were added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import About from "./components/about";
+import Links from "./components/link";
+import Works from "./components/works";
+import Contact from "./components/contact";
+import { Container } from "@mui/material";
+import Home from "./components/home";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Links />
+      <Container>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
-
-export default App;
